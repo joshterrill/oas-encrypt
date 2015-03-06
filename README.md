22eb4a8b5e19a7d475f40ff725ddffe7096fc253
Joshua Terrill <joshterrill.dev@gmail.com>
2015-03-06 23:53:32 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# oasCrypt
+a simple substitution encryption module
+
+### installation
+```
+npm install oasCrypt
+```
+
+### usage
+```
+var oasCrypt = require("oascrypt");
+oasCrypt.encode("testing123");
+
+output: ZTLZOFU)(* 
+```
---
+++ index.js
@@ -0,0 +1,21 @@
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+encoded = "QWERTYUIOPASDFGHJKLZXCVBNM)(*&^%$#@1";
+ 
+var word = "";
+
+exports.encode = function(word) {
+  word = word.toUpperCase();
+  
+  result = "";
+  
+  i=0;
+
+  while (i < word.length) {
+    ind = alphabet.indexOf(word.charAt(i));
+    result = result + encoded.charAt(ind);
+    i++;
+  }
+  
+  console.log(result);
+  
+}
